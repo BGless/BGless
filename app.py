2d360747549ef69aebf34ac2fb53a2046e7006f1
herotux <thefreetux@gmail.com>
2021-04-12 16:40:17 +0430

    get file and modify them

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import cv2 as cv
 import numpy as np 
 import matplotlib.pyplot as plt
-
+import cvclass 
 from flask import Flask, render_template
-
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -12,6 +12,24 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/remove", methods=["POST"])
+def remove():
 
+    start = time.time()
+
+    if 'file' not in request.files:
+        return jsonify({'error': 'missing file'}), 400
+
+    if request.files['file'].filename.rsplit('.', 1)[1].lower() not in ["jpg", "png", "jpeg"]:
+        return jsonify({'error': 'invalid file format'}), 400
+
+    data = request.files['file'].read()
+    
+    if len(data) == 0:
+        return jsonify({'error': 'empty image'}), 400
+
+    img = Image.open(io.BytesIO(data))
+    remove = cvclass.crt(img)
+    return remove
 
 
--- cvclass.py
+++ cvclass.py
@@ -7,14 +7,4 @@ class cvclass:
     def crt(self, upimg):
         img = cv.imread(upimg)
         gray_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-
-fig, ax = plt.subplots(1, 2, figsize=(16, 8))
-fig.tight_layout()
-
-ax[0].imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
-ax[0].set_title("Original")
-
-ax[1].imshow(cv.cvtColor(gray_image, cv.COLOR_BGR2RGB))
-ax[1].set_title("Grayscale")
-plt.show()
+        
